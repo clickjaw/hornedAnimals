75eecf10a781f2081ca93df33b8d2719188d052d
Tyler Morgan <clickjaw@outlook.com>
2022-08-10 09:51:13 -0500

    pictures

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import Main from "./components/Main";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+export default class App extends Component {
+  render() {
+    return (
+      <>
+      <Header/>
+      <Main/>
+      <Footer/>
+      
+      
+      </>
+    )
+  }
+}
